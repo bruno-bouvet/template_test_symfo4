8d16df457d82c850b5855cd3b2b8fdaa06b1261c
bruno-bouvet <bouvet.bruno@gmail.com>
2019-04-04 18:30:04 +0200

    feature: added search filter

---
+++ src/Entity/PropertySearch.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bruno
+ * Date: 2019-04-04
+ * Time: 17:07
+ */
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PropertySearch
+{
+    /**
+     * @var int|null
+     */
+    private $maxPrice;
+
+    /**
+     * @Assert\Range(min="10", max="400")
+     * @var int|null
+     */
+    private $minSurface;
+
+    /**
+     * @param int|null $maxPrice
+     * @return PropertySearch
+     */
+    public function setMaxPrice(int $maxPrice): PropertySearch
+    {
+        $this->maxPrice = $maxPrice;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMaxPrice(): ?int
+    {
+        return $this->maxPrice;
+    }
+
+    /**
+     * @param int|null $minSurface
+     * @return PropertySearch
+     */
+    public function setMinSurface(int $minSurface): PropertySearch
+    {
+        $this->minSurface = $minSurface;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMinSurface(): ?int
+    {
+        return $this->minSurface;
+    }
+
+
+}
