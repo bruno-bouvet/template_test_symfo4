01b961ea7a215bc32c1d71bd1f089cdafbede54e
bruno-bouvet <bouvet.bruno@gmail.com>
2019-04-10 17:21:18 +0200

    feature:added email contact notification

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -9,9 +9,12 @@
 namespace App\Controller;
 
 
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Entity\PropertySearch;
+use App\Form\ContactType;
 use App\Form\PropertySearchType;
+use App\Notification\ContactNotification;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
@@ -19,6 +22,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class PropertyController extends AbstractController
 {
@@ -48,7 +54,7 @@ class PropertyController extends AbstractController
      * @return Response
      * @Route("/biens", name="property.index")
      */
-    public function index(PaginatorInterface $paginator,  Request $request): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
         $search = new PropertySearch();
         $form = $this->createForm(PropertySearchType::class, $search);
@@ -69,22 +75,47 @@ class PropertyController extends AbstractController
     /**
      * @param Property $property
      * @param string $slug
+     * @param Request $request
+     * @param ContactNotification $notification
      * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug" : "[a-z0-9\-]*" })
      */
-    public function show(Property $property, string $slug): Response
+    public function show(Property $property, string $slug, Request $request, ContactNotification $notification): Response
     {
-        if ($property->getslug() !== $slug)
-        {
+        if ($property->getslug() !== $slug) {
             return $this->redirectToRoute('property.show', [
                 'id' => $property->getId(),
                 'slug' => $property->getSlug()
             ], 301);
         }
 
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+
+            unset($form);
+            $contact = new Contact();
+            $form = $this->createForm(ContactType::class, $contact);
+
+//            return $this->redirectToRoute('property.show', [
+//                'id' => $property->getId(),
+//                'slug' => $property->getSlug()
+//            ]
+//            );
+        }
+
         return $this->render('property/show.html.twig', [
             'current_menu' => 'properties',
-            'property' => $property
+            'property' => $property,
+            'form' => $form->createView()
         ]);
     }
 
